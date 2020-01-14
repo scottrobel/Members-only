a108d65b39a79d1dcd3a6be85a22a64a495b4866
scottrobel <robelrain@gmail.com>
2020-01-13 20:33:48 -0500

    fix password confimarion

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, length: {within: 6..40}, allow_blank: true
   validates :username, uniqueness: true, allow_blank: true
-  validates :password, confirmation: true,
-                       length: {within: 6..40},
-                       allow_blank: true,
-                       on: :update
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "not valid" }, uniqueness: true, allow_blank: true
   has_secure_password
