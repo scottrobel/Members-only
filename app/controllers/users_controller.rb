e2657aed4c4418077dcab11d0e7f9426d2e265de
scottrobel <robelrain@gmail.com>
2020-01-14 18:03:21 -0500

    add login method to User#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      login_as @user
       flash[:success] = "Sign up successful"
       redirect_to profile_path
     else
